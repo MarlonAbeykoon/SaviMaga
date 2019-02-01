a322fa484c6c67f1beab510813de55198e951743
Prince Fernando <prince.fernando@gmail.com>
2019-02-01 21:29:43 +0530

    [Fixed] Adding proper placeholder: New Applicant>Phone No 2

--- main/web/newApplicant.php
+++ main/web/newApplicant.php
@@ -174,11 +174,11 @@ if ($id == '0') {
 
             <div class="form-group row">
 
-                <label class="control-label text-right col-md-3">Phone No2</label>
+                <label class="control-label text-right col-md-3">Phone No 2</label>
 
                 <div class="col-md-9">
 
-                    <input type="text" class="form-control" name="phone2" placeholder="123456789" pattern="[0-9]{10}" title="10 Digit Phone No">
+                    <input type="text" class="form-control" name="phone2" placeholder="Phone 2" pattern="[0-9]{10}" title="10 Digit Phone No">
 
                     <small class="form-control-feedback"></small> </div>
 
